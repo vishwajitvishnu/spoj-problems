c16aef1dc7bd9ce980bf58a5a5af4cb6827000f1
vishwajit kumar vishnu <vishwajit_vishnu@hotmail.com>
2015-02-12 23:37:06 +0530

    THE MAX LINES ( a spoj problem)
    http://www.spoj.com/problems/MAXLN/

---
+++ maxln.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+main(){
+	long long t, r,temp,i;
+	cin>>t;
+	for(i=1;i<=t;i++){
+		cin>>r;
+		temp=4*r*r;
+		cout<<"Case "<<i<<": "<<temp<<".25\n";
+	}	
+}
